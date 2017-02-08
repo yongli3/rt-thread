277b02afd5a810ff882c80d43c2494bd383ca80a
Yong Li <sdliyong@gmail.com>
2017-02-08 08:53:13 +0800

    Add test doc
    Signed-off-by: Yong Li <sdliyong@gmail.com>

--- mk.sh
+++ mk.sh
@@ -1,4 +1,9 @@
+# build .axf using the below commands, and use Ozone to flash/debug
 cd bsp/stm32f40x/
-
 scons --verbose --clean
 scons --verbose
+
+# After booting, use msh command to switch msh,
+# then ifconfig to check the IP address
+# After get IP, run tcpserv to start tcp server on 5000
+# On Linux client, nc <ip> 5000 to connect to board. the uart2 is connect to TCP client
